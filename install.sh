16d9967bbd1daec18c6121648d00b1c44b23f2b7
jaewon <jaewon.lee@deepingsource.io>
2022-01-22 11:20:25 +0900

    homebrew install

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Pycharm install
+brew install --cask pycharm
+brew install --cask clion
+brew install --cask notion
+
+# Iterm
+brew install --cask iterm
+brew install --cask slack
+brew install --cask cleanmymac
+
+
